3cd8ebe6d90bedce5ecc2326d62a891d7063d79e
James Jung <jjng930@gmail.com>
2022-04-06 13:27:22 -0500

    created Pseudo code

---
+++ app.js
@@ -0,0 +1,28 @@
+/*
+1) Assigning global variables / DOM
+-player
+-dealer
+-scores set to 0
+- values to "Ace" & "Face cards"
+
+2) Creating arrays
+-deck 
+-suits
+
+3) addEventListener to buttons
+-start game - deal cards when it's clicked
+-new game - reset game including scoreboard 
+-hit - pulling a card from the array
+-stay - keep current cards and let dealer hit 
+
+4) Functions 
+-startGame() - Main function will load the entire game
+-add up the values when the cards are dealt
+-pulling random card from the array / randomize player and dealer cards
+
+5) Winning/losing/draw
+-decide the winning, losing and draw conditions / including "bust" condition
+-increment/decrement scores
+
+
+*/

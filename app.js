c754c923ad12a5ceae327c7dcff830fae6256275
James Jung <jjng930@gmail.com>
2022-04-08 16:22:30 -0500

    new js code

--- app.js
+++ app.js
@@ -28,50 +28,142 @@
 5) Winning/losing/draw
 -decide the winning, losing and draw conditions / including "bust" condition
 
+
+
 */
+//---------------------------------------------------------------------------------
+// ****** DOM VARIABLES ******
+//---------------------------------------------------------------------------------
+// let dText = document.querySelector('h2');
+// let pText = document.querySelector('h3');
+// let text = document.querySelector('.text');
+// let dealBtn = document.querySelector('#deal_btn');
+// let hitBtn = document.querySelector('#hit_btn');
+// let standBtn = document.querySelector('#stand_btn');
+// let newGameBtn = document.querySelector('#new_btn');
+// let dCards = document.querySelector('#dealer_cards');
+// let pCards = document.querySelector('#player_cards');
+// console.log(deal)
 
-function buildDeck() {
-    // iterates through each card and suit and pushed into the empty array of "deck"
-    for (let i = 0; i < suits.length; i++) {
-        for (let j = 0; j < cards.length; j++) {
-            deck.push(
-                {card: cards[j],
-                suit: suits[i],
-                value: values[j]}
-            )
-        }
-    }
-    console.log(deck);
-}
+//---------------------------------------------------------------------------------
+// ****** GAME VARIABLES ******
+//---------------------------------------------------------------------------------
 
+// // let gameStart = false;
+// let onHit = true;
+// let dealerSum = 0;
+// let playerSum = 0;
+// let dealerAceCount = 0;
+// let playerAceCount = 0;
+// let cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+// let suits = ["♣", "♦", "♥", "♠"];
+// let values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10 , 10, 10];
+// let deck = [];
+// let hand = 0;
+// let dealerCards = [];
+// let playerCards = [];
+// let currentDealerHand = null;
+// let currentDealerHand1 = null;
+// let currentPlayerHand = null;
+// let currentPlayerHand1 = null;
+
+// //---------------------------------------------------------------------------------
+// // ****** BUTTONS ******
+// //---------------------------------------------------------------------------------
+
+// // Start the game
+// dealBtn.addEventListener('click', () => {
+//     getCard();
+//     dCards.innerHTML = currentDealerHand.card + currentDealerHand.suit + "  " + currentDealerHand1.card + currentDealerHand1.suit;
+//     pCards.innerHTML = currentPlayerHand.card + currentPlayerHand.suit + "  " + currentPlayerHand1.card + currentPlayerHand1.suit;
+//     text.innerHTML = ("Cards are dealt!");
+//     dealerSum = currentDealerHand.value + currentDealerHand1.value;
+//     playerSum = currentPlayerHand.value + currentPlayerHand1.value;
+//     dText.innerHTML = (`Dealer has: ${dealerSum}`);
+//     pText.innerHTML = (`Player has: ${playerSum}`);
+// })
+
+// hitBtn.addEventListener('click', () => {
+    
+// })
+
+// // Reset the game when button is clicked
+// newGameBtn.addEventListener('click', () => {
+//     dCards.innerHTML = "";
+//     pCards.innerHTML = "";
+//     dText.innerHTML = "Dealer";
+//     pText.innerHTML = "Player";
+//     text.innerHTML = 'Press "Deal" to play';
+//     dealerSum = 0;
+// })
+
+// //---------------------------------------------------------------------------------
+// // ****** FUNCTIONS ******
+// //---------------------------------------------------------------------------------
 // buildDeck()
-
-function shuffleDeck(deck) {
-    for (let i = 0; i < deck.length; i++) {
-        let j = Math.floor(Math.random() * deck.length);
-        // Math.random() * deck.length will give a random float number between (0 - 52)
-        // Math.floor will round it to an integer
-
-        let swap = deck[i]; // grabbing the first element(deck[i])
-
-        deck[i] = deck[j]; 
-        deck[j] = swap; 
-        // replacing the first element(deck[i]) with the second element(deck[j])
-    }
-    // console.log(deck)
-}
-
 // shuffleDeck(deck)
+// distributeCards(deck)
 
-function distributeCards(deck) {
-    // adds the first 26 cards into the "dealerCards" array
-    for (let i = 0; i < 26; i++) {
-        dealerCards.push(deck[i])
-    }
-    // adds the last 26 cards into the "playerCards" array
-    for (let i = 26; i < 52; i++) {
-        playerCards.push(deck[i])
-    }
-    console.log(dealerCards)
-    console.log(playerCards)
-}
+// function buildDeck() {
+//     // iterates through each card and suit and pushed into the empty array of "deck"
+//     for (let i = 0; i < suits.length; i++) {
+//         for (let j = 0; j < cards.length; j++) {
+//             deck.push(
+//                 {card: cards[j],
+//                 suit: suits[i],
+//                 value: values[j]}
+//             )
+//         }
+//     }
+//     console.log(deck);
+// }
+
+// // buildDeck()
+
+// function shuffleDeck(deck) {
+//     for (let i = 0; i < deck.length; i++) {
+//         let j = Math.floor(Math.random() * deck.length);
+//         // Math.random() * deck.length will give a random float number between (0 - 52)
+//         // Math.floor will round it to an integer
+
+//         let swap = deck[i]; // grabbing the first element(deck[i])
+
+//         deck[i] = deck[j]; 
+//         deck[j] = swap; 
+//         // replacing the first element(deck[i]) with the second element(deck[j])
+//     }
+//     // console.log(deck)
+// }
+
+// // shuffleDeck(deck)
+
+// function distributeCards(deck) {
+//     // adds the first 26 cards into the "dealerCards" array
+//     for (let i = 0; i < 26; i++) {
+//         dealerCards.push(deck[i])
+//     }
+//     // adds the last 26 cards into the "playerCards" array
+//     for (let i = 26; i < 52; i++) {
+//         playerCards.push(deck[i])
+//     }
+//     console.log(dealerCards)
+//     console.log(playerCards)
+// }
+
+// function getCard() {
+//     // console.log(hand)
+//     // grabbing the objects of the dealerCards array
+//     currentDealerHand = dealerCards[hand]
+//     currentPlayerHand = playerCards[hand]
+
+//     hand++ // incrementing the objects inside the array
+
+//     currentDealerHand1 = dealerCards[hand]
+//     currentPlayerHand1 = playerCards[hand]
+// }
+
+// function hitMe() {
+//     if (playerSum < 21) {
+
+//     }
+// }
---
+++ blackjack.js
@@ -0,0 +1,125 @@
+// ----------------------------------------------------------------
+// ****************** GLOBAL VARIABLES ******************
+// ----------------------------------------------------------------
+let cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+let suits = ["♣", "♦", "♥", "♠"];
+let values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10 , 10, 10];
+let deck = [];
+let gameStart = false;
+let gameOver = false;
+let dealerWin = false;
+let playerWin = false;
+let dealerSum = 0;
+let playerSum = 0;
+let dealerCards = [];
+let dealerInPlayHand = [];
+let playerInPlayHand = [];
+let playerCards = [];
+let newGameBtn = document.getElementById("new_btn");
+let hitBtn = document.getElementById("hit_btn");
+let dealBtn = document.getElementById("deal_btn")
+let dealerText = document.getElementById("dealer_cards")
+let playerText = document.getElementById("player_cards")
+let x = "";
+// ----------------------------------------------------------------
+// ****************** BUTTONS ******************
+// ----------------------------------------------------------------
+
+dealBtn.addEventListener("click", () => {
+  gameStart = true;
+  gameOver = false;
+  playerWin = false;
+
+  buildDeck();
+  shuffleDeck(deck);
+  distributeCards(deck);
+
+  dealerInPlayHand = [dealerCards.shift(), dealerCards.shift()];
+  playerInPlayHand = [playerCards.shift(), playerCards.shift()];
+  dealerText.innerHTML = (dealerInPlayHand[0].card + dealerInPlayHand[0].suit + " " + dealerInPlayHand[1].card + dealerInPlayHand[1].suit)
+  playerText.innerHTML = (playerInPlayHand[0].card + playerInPlayHand[0].suit + " " + playerInPlayHand[1].card + playerInPlayHand[1].suit)
+  console.log(dealerInPlayHand)
+  console.log(playerInPlayHand)
+});
+
+hitBtn.addEventListener('click', () => {
+  playerInPlayHand.push(playerCards.shift());
+  console.log(playerInPlayHand)
+})
+
+// ----------------------------------------------------------------
+// ****************** FUNCTIONS ******************
+// ----------------------------------------------------------------
+function buildDeck() {
+    // iterates through each card and suit and pushed into the empty array of "deck"
+    for (let i = 0; i < suits.length; i++) {
+        for (let j = 0; j < cards.length; j++) {
+            deck.push(
+                {card: cards[j],
+                suit: suits[i],
+                value: values[j]}
+            )
+        }
+    }
+    return deck;
+}
+
+// buildDeck()
+
+function shuffleDeck(deck) {
+    for (let i = 0; i < deck.length; i++) {
+        let j = Math.floor(Math.random() * deck.length);
+        // Math.random() * deck.length will give a random float number between (0 - 52)
+        // Math.floor will round it to an integer
+
+        let swap = deck[i]; // grabbing the first element(deck[i])
+
+        deck[i] = deck[j]; 
+        deck[j] = swap; 
+        // replacing the first element(deck[i]) with the second element(deck[j])
+    }
+    console.log(deck)
+}
+
+// shuffleDeck(deck)
+
+function distributeCards(deck) {
+    // adds the first 26 cards into the "dealerCards" array
+    for (let i = 0; i < 26; i++) {
+        dealerCards.push(deck[i])
+    }
+    // adds the last 26 cards into the "playerCards" array
+    for (let i = 26; i < 52; i++) {
+        playerCards.push(deck[i])
+    }
+    console.log(dealerCards)
+    console.log(playerCards)
+}
+
+function gameStatus() {
+  if (gameStart === false) {
+    return;
+  }
+
+  // displaying current cards for dealer and player
+  for (let i = 0; i < dealerCards.length; i++) {
+  
+  }
+  console.log(currentDealerCards)
+  for (let i = 0; i < playerCards.length; i++) {
+    //something innerHtml
+    //display currentBoardLayout
+  }
+
+  //update scores...
+
+  if (gameOver) {
+    if (playerWin) {
+      //display "YOU WIN"
+    } else {
+      //display "DEALER WINS"
+    }
+  }
+}
+
+// gameStatus();

a81d0c58d811256760760ad150a6ce8290623128
James Jung <jjng930@gmail.com>
2022-04-11 15:23:15 -0500

    pseudo.js added

---
+++ pseudo.js
@@ -0,0 +1,31 @@
+/*
+
+** homepage will have a play button that will transition into the game **
+
+1) Assigning global variables / DOM
+-player
+-dealer
+- values to "Ace" & "Face cards"
+
+2) Creating arrays
+-deck = []
+-suits = []
+
+3) addEventListener to buttons
+-play - play button will load the game page
+-deal - deal cards when it's clicked
+-new game - reset game including scoreboard 
+-hit - pulling a card from the array
+-stand - keep current cards and let dealer hit 
+
+4) Functions 
+-startGame() - Main function will load the entire game
+-build a deck - get combinations of every card/suit
+-shuffle deck - mix the array of the decks
+-add up the values when the cards are dealt
+-pulling random card from the array / randomize player and dealer cards
+
+5) Winning/losing/draw
+-decide the winning, losing and draw conditions / including "bust" condition
+
+*/
